48586d9f6e03064dbc0e0362e57e228877e97dea
Bartek <btmikolajczyk@wp.pl>
2020-01-23 00:55:15 +0100

    post_delete test

--- testy/rest.test/src/main/java/rest/test/RestCrudTest.java
+++ testy/rest.test/src/main/java/rest/test/RestCrudTest.java
@@ -4,7 +4,6 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.primefaces.json.JSONArray;
 import org.primefaces.json.JSONException;
 import org.primefaces.json.JSONObject;
@@ -12,62 +11,42 @@ import org.primefaces.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 
 public class RestCrudTest {
 
     public static String url = "https://localhost:8443/pas/api/items";
+    public int size = 0;
 
-//    @Test public void getItemList() throws JSONException {
-//        Response response = RestAssured.given().
-//                relaxedHTTPSValidation().
-//                auth().
-//                basic("Superuser", "admin").
-//                when().
-//                get(url);
-//        Assertions.assertEquals(new JSONArray(response.asString()).length(), 21);
-//    }
+    @Test public void getItemList() throws JSONException {
+        Response response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url);
+        size = new JSONArray(response.asString()).length();
+        Assertions.assertNotEquals(size, 0);
+    }
 
-//    @Test public void getItemSingle() throws JSONException {
-//        Response response = RestAssured.given().
-//                relaxedHTTPSValidation().
-//                auth().
-//                basic("Superuser", "admin").
-//                when().
-//                get(url + "/0");
-//        JSONObject body = new JSONObject(response.asString());
-//        Assertions.assertEquals(body.get("id"), 0);
-//    }
+    @Test public void getItemSingle() throws JSONException {
+        Response response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url + "/0");
+        JSONObject body = new JSONObject(response.asString());
+        Assertions.assertEquals(body.get("id"), 0);
+    }
 
 
-//    @Test public void deleteItem() throws JSONException {
-//        RestAssured.given().
-//                relaxedHTTPSValidation().
-//                auth().
-//                basic("Superuser", "admin").
-//                when().
-//                post(url).
-//                body();
-//        Response response = RestAssured.given().
-//                relaxedHTTPSValidation().
-//                auth().
-//                basic("Superuser", "admin").
-//                when().
-//                delete(url);
-//    }
-//
-//
 //    @Test public void putItem() throws JSONException {
-//        Response response = RestAssured.given().
-//                relaxedHTTPSValidation().
-//                auth().
-//                basic("Superuser", "admin").
-//                when().
-//                put(url);
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put("Content-Type", "application/json");
+//
 //    }
 
-    @Test public void postItem() throws JSONException {
-
+    @Test public void post_deleteItem() throws JSONException {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
@@ -79,7 +58,8 @@ public class RestCrudTest {
                         .put("stickerPoints", 100))
                 .put("available", true)
                 .put("price", 12.21)
-                .put("releaseDate", 869233)
+                .put("releaseDate", 4359)
+                .put("title", "title")
                 .put("author", "autor")
                 .put("genre", "POP")
                 .put("tracks", 20);
@@ -97,8 +77,22 @@ public class RestCrudTest {
                 basic("Superuser", "admin").
                 when().
                 get(url);
-        Assertions.assertEquals(responsePost.getStatusCode(), 400);
+        Assertions.assertEquals(responsePost.getStatusCode(), 200);
         Assertions.assertEquals(response.getStatusCode(), 200);
-        Assertions.assertEquals(new JSONArray(response.asString()).length(), 22);
+        Assertions.assertEquals(new JSONArray(response.asString()).length(), size+1);
+        Response responseDelete = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                delete(url + "/" + (new JSONArray(response.asString()).length() - 1));
+        Assertions.assertEquals(responseDelete.getStatusCode(), 204);
+        response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url);
+        Assertions.assertEquals(new JSONArray(response.asString()).length(), size);
     }
 }
