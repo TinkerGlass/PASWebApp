8472e13aee9f8bcfc3e9bca6ba2207c47610595a
4kaze <32jacke32@gmail.com>
2020-01-23 01:28:05 +0100

    add status tests

--- testy/rest.test/src/main/java/rest/test/RestCrudTest.java
+++ testy/rest.test/src/main/java/rest/test/RestCrudTest.java
@@ -122,4 +122,88 @@ public class RestCrudTest {
                 get(url);
         Assertions.assertEquals(new JSONArray(response.asString()).length(), size);
     }
+
+    @Test public void postConflict() throws JSONException {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        JSONObject body = new JSONObject()
+                .put("type", "album")
+                .put("Sticker", new JSONObject()
+                        .put("stickerContent", "content")
+                        .put("stickerName", "name")
+                        .put("stickerPoints", 100))
+                .put("available", true)
+                .put("price", 12.21)
+                .put("releaseDate", 4359)
+                .put("title", "title")
+                .put("author", "autor")
+                .put("genre", "POP")
+                .put("tracks", 20)
+                .put("id", 0);
+        Response responsePost = RestAssured.given().
+                relaxedHTTPSValidation().
+                headers(headers).
+                auth().
+                basic("Superuser", "admin").
+                body(body.toString()).
+                when().
+                post(url);
+        Assertions.assertEquals(responsePost.getStatusCode(), 409);
+    }
+
+    @Test public void getItemNotFound() throws JSONException {
+        Response response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url);
+        size = new JSONArray(response.asString()).length();
+
+        response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url + "/" + (size+10));
+        Assertions.assertEquals(response.getStatusCode(), 404);
+    }
+
+    @Test public void putItemNotFound() throws JSONException {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        Response response = RestAssured.given().
+                relaxedHTTPSValidation().
+                auth().
+                basic("Superuser", "admin").
+                when().
+                get(url);
+        size = new JSONArray(response.asString()).length();
+
+        JSONObject body = new JSONObject()
+                .put("type", "album")
+                .put("Sticker", new JSONObject()
+                        .put("stickerContent", "content")
+                        .put("stickerName", "name")
+                        .put("stickerPoints", 100))
+                .put("available", true)
+                .put("price", 12.21)
+                .put("releaseDate", 4359)
+                .put("title", "title")
+                .put("author", "autor")
+                .put("genre", "POP")
+                .put("tracks", 20);
+
+        response = RestAssured.given().
+                relaxedHTTPSValidation().
+                headers(headers).
+                auth().
+                basic("Superuser", "admin").
+                body(body.toString()).
+                when().
+                put(url + "/" + (size+10));
+        Assertions.assertEquals(response.getStatusCode(), 404);
+    }
 }
